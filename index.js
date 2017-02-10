f76beefa3f04a54e3415a66ebdd0b3413b2bcd52
genericProgramming <bencschulz@gmail.com>
2017-02-10 17:45:16 -0600

    adding some cleanup and schedule validation

--- index.js
+++ index.js
@@ -6,82 +6,91 @@ var later = require('later');
 // Load configuration 
 var fs = require('fs');
 var configFileLocation = process.argv[2] || 'configuration.json'
-console.log("Using configuration file ", configFileLocation)
+console.log('Using configuration file', configFileLocation)
 var configuration = JSON.parse(fs.readFileSync(configFileLocation, 'utf8'));
 // TODO validate this dude ^^
 
 // configure the schedules 
 later.date.localTime();
 
-console.log("Start time schedule is: ", configuration.start_time_schedule)
-console.log("End time schedule is: ", configuration.end_time_schedule)
+console.log('Start time schedule is: ', configuration.start_time_schedule)
+console.log('End time schedule is: ', configuration.end_time_schedule)
 
 var start_schedule = later.parse.text(configuration.start_time_schedule)
+if (start_schedule.error != -1){
+	console.log(start_schedule)
+	return -1
+}
 var end_schedule = later.parse.text(configuration.end_time_schedule)
+if (end_schedule.error != -1){
+	console.log(end_schedule)
+	return -1
+}
 
 // schedule the shit
 later.setInterval(eventuallyTakeActionOnAChromecast(loadTheMedia, configuration), start_schedule)
 later.setInterval(eventuallyTakeActionOnAChromecast(stopTheMedia, configuration), end_schedule)
 
 function eventuallyTakeActionOnAChromecast(action, configuration) {
-    return () => {
-        console.log('Taking some action');
-        var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+	return () => {
+		console.log('Taking an action');
+		var browser = mdns.createBrowser(mdns.tcp('googlecast'));
 
-        browser.on('serviceUp', function(service) {
-            if (service.txtRecord && service.txtRecord.fn === configuration.device_name) {
-                action(service.addresses[0], configuration);
-            }
-            browser.stop();
-        });
-        browser.start();
-    }
+		browser.on('serviceUp', function(service) {
+			if (service.txtRecord && service.txtRecord.fn === configuration.device_name) {
+				action(service.addresses[0], configuration);
+				browser.stop();
+			}
+			else {
+				console.log('Unable to find a chrome cast for name ',configuration.device_name, ' for service ',service.txtRecord)
+			}
+		});
+		browser.start();
+	}
 }
 
 function loadTheMedia(host, configuration) {
-    connectToTheChromecast(host, function(client) {
-        console.log('connected, launching app ...');
+	connectToTheChromecast(host, function(client) {
+		console.log('connected, launching app ...');
 
-        client.launch(DefaultMediaReceiver, function(err, player) {
-            player.load(configuration.media, {
-                autoplay: true
-            }, function(err, status) {
-                console.log('media loaded playerState=%s', status.playerState)
-                // disconnect cause who cares?
-                client.close();
-            });
+		client.launch(DefaultMediaReceiver, function(err, player) {
+			player.load(configuration.media, {
+				autoplay: true
+			}, function(err, status) {
+				console.log('media loaded playerState=%s', status.playerState)
+			});
 
-            player.on('status', function(status) {
-                console.log('status broadcast playerState=%s', status.playerState);
-            });
-        });
-    });
+			player.on('status', function(status) {
+				console.log('status broadcast playerState=%s', status.playerState);
+			});
+		});
+	});
 }
 
 function connectToTheChromecast(host, action) {
-    var client = new Client();
+	var client = new Client();
 
-    client.connect(host, () => {
-        action(client)
-    });
+	client.connect(host, () => {
+		action(client)
+	});
 
-    client.on('error', function(err) {
-        console.log('Error: %s', err.message);
-        client.close();
-    });
+	client.on('error', function(err) {
+		console.log('Error: %s', err.message);
+		client.close();
+	});
 }
 
 function stopTheMedia(host, configuration) {
-    connectToTheChromecast(host, function(client) {
-        console.log('connected, stopping app ...');
+	connectToTheChromecast(host, function(client) {
+		console.log('connected, stopping app ...');
 
-        client.launch(DefaultMediaReceiver, function(err, player) {
-            player.stop(function(status) {
-                console.log("Done!", status);
-            });
-            player.on('status', function(status) {
-                console.log('status broadcast playerState=%s', status.playerState);
-            });
-        });
-    })
+		client.launch(DefaultMediaReceiver, function(err, player) {
+			player.stop(function(status) {
+				console.log("Done!", status);
+			});
+			player.on('status', function(status) {
+				console.log('status broadcast playerState=%s', status.playerState);
+			});
+		});
+	})
 }
